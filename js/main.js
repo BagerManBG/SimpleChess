68538f5934c0144d4a2e263092f573ddffba1b52
BagerManBG <toshko_1998@abv.bg>
2017-06-26 15:11:45 +0300

    top/figure/ and knight movements

--- js/main.js
+++ js/main.js
@@ -23,10 +23,13 @@ $(document).ready(function(){
 	});
 
 	$('#main-content .field img').bind("mouseleave", function(){
-		//console.log(greenDots[0]);
+		
 		for (var i = 0; i < greenDots.length; i++) {
+
 			$('.field#' + greenDots[i]).removeClass('greenBG');
+			$(this).parent().removeClass('selectedPiece');
 		}
+
 		greenDots.length = 0;
 	});
 });
@@ -59,13 +62,13 @@ function initializeField() {
 	$('.field#0_1, .field#0_6').append("<img src='resources/images/pieces/b_k.svg' id='b_k' class='b'>");
 	$('.field#0_2, .field#0_5').append("<img src='resources/images/pieces/b_o.svg' id='b_o' class='b'>");
 	$('.field#0_3').append("<img src='resources/images/pieces/b_q.svg' id='b_q' class='b'>");
-	$('.field#0_4').append("<img src='resources/images/pieces/b_kg.svg' id='b_kg' class='b'>");
+	$('.field#0_4').append("<img src='resources/images/pieces/b_g.svg' id='b_g' class='b'>");
 
 	$('.field#7_0, .field#7_7').append("<img src='resources/images/pieces/w_t.svg' id='w_t' class='w'>");
 	$('.field#7_1, .field#7_6').append("<img src='resources/images/pieces/w_k.svg' id='w_k' class='w'>");
 	$('.field#7_2, .field#7_5').append("<img src='resources/images/pieces/w_o.svg' id='w_o' class='w'>");
 	$('.field#7_3').append("<img src='resources/images/pieces/w_q.svg' id='w_q' class='w'>");
-	$('.field#7_4').append("<img src='resources/images/pieces/w_kg.svg' id='w_kg' class='w'>");
+	$('.field#7_4').append("<img src='resources/images/pieces/w_g.svg' id='w_g' class='w'>");
 
 	for (var i = 0; i < 8; i++) {
 		
@@ -132,9 +135,12 @@ function findPossibleMoves(piece, x, y, color, type) {
 	switch(type) {
 
 		case 'p':
-			greenX.push( eval(x + sign + '1') );
-			greenY.push(y);
-			dots++;
+			if(x + 1 <= 7 || x - 1 >= 0) {
+
+				greenX.push( eval(x + sign + '1') );
+				greenY.push(y);
+				dots++;
+			}
 
 			if (turn == 1) {
 
@@ -143,6 +149,71 @@ function findPossibleMoves(piece, x, y, color, type) {
 				dots++;
 			}
 			break;
+
+		case 'k':
+			if(x + 2 <= 7) {
+
+				if(y + 1 <= 7) {
+
+					greenX.push(x+2); greenY.push(y+1);
+					dots++;
+				}
+
+				if(y - 1 >= 0) {
+
+					greenX.push(x+2); greenY.push(y-1);
+					dots++;
+				}
+			}
+
+			if(x - 2 >= 0) {
+
+				if(y + 1 <= 7) {
+
+					greenX.push(x-2); greenY.push(y+1);
+					dots++;
+				}
+
+				if(y - 1 >= 0) {
+
+					greenX.push(x-2); greenY.push(y-1);
+					dots++;
+				}
+			}
+			break;
+
+		case 't':
+			/* upwards */
+			for (var i = x - 1; i >= 0; i--) {
+				
+				greenX.push(i);
+				greenY.push(y);
+				dots++;
+			}
+
+			/* downwards */
+			for (var i = x + 1; i <= 7 - x; i++) { 
+				
+				greenX.push(i);
+				greenY.push(y);
+				dots++;
+			}
+
+			/* left */
+			for (var i = y - 1; i >= 0; i--) {
+				
+				greenX.push(x);
+				greenY.push(i);
+				dots++;
+			}
+
+			/* right */
+			for (var i = y + 1; i <= 7 - y; i++) { 
+				
+				greenX.push(x);
+				greenY.push(i);
+				dots++;
+			}
 
 		default:
 			break;
@@ -153,49 +224,6 @@ function findPossibleMoves(piece, x, y, color, type) {
 		greenDots.push( greenX[i] + '_' + greenY[i] );
 
 		$('.field#' + greenDots[i]).addClass('greenBG');
+		piece.parent().addClass('selectedPiece');
 	}
-
-	/*
-	if (id == 'w_p') {
-
-		if (turn == 1) {
-
-			greenX = x - 2;
-			greenDots.push($('.field#' + greenX + '_' + y).attr('id'));
-			greenX = x - 1;
-			greenDots.push($('.field#' + greenX + '_' + y).attr('id'));
-		} else {
-
-			greenX = x - 1;
-			greenDots.push($('.field#' + greenX + '_' + y).attr('id'));
-		}
-
-		for (var i = 0; i < greenDots.length; i++) {
-
-			$('.field#' + greenDots[i]).addClass('greenBG');
-		}
-
-		greenDots.push($('.field#' + greenX + '_' + y).attr('id'));
-	}
-
-	if (id == 'b_p') {
-
-		if (turn == 1) {
-
-			greenX = x + 2;
-			greenDots.push($('.field#' + greenX + '_' + y).attr('id'));
-			greenX = x + 1;
-			greenDots.push($('.field#' + greenX + '_' + y).attr('id'));
-		} else {
-
-			greenX = x + 1;
-			greenDots.push($('.field#' + greenX + '_' + y).attr('id'));
-		}
-
-		for (var i = 0; i < greenDots.length; i++) {
-
-			$('.field#' + greenDots[i]).addClass('greenBG');
-		}	
-	}
-	*/
 }
